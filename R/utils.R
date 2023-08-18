317270b28cfef70b1eec7f0aab4ecd70482466d8
Thierry Moudiki <thierry.moudiki@gmail.com>
2023-08-19 00:36:19 +0200

    add function for splitting time series

--- R/utils.R
+++ R/utils.R
@@ -339,3 +339,41 @@ scale_ahead <- function(x, center = TRUE, scale = TRUE)
   tsp(x) <- tspx
   return(x)
 }
+
+# Split a time series -----
+splitts <- function(y, p = 0.8, return_indices = TRUE, ...)
+{
+    n_y <- base::ifelse(test = is.null(dim(y)),
+                      yes = length(y),
+                      no = dim(y)[1])
+
+    index_train <- 1:floor(p*n_y)
+    if (return_indices)
+      return(index_train)
+
+    start_y <- stats::start(y)
+    frequency_y <- stats::frequency(y)
+
+    if(is.null(ncol(y))) # univariate case
+    {
+        training <- ts(y[index_train],
+                       start = start_y,
+                       frequency = frequency_y)
+        start_testing <- tsp(training )[2] + 1 / frequency_y
+        return(list(training = training,
+                    testing = ts(y[-index_train],
+                                 start = start_testing,
+                                 frequency = frequency_y)))
+    } else { # multivariate case
+      training <- ts(y[index_train, ],
+                     start = start_y,
+                     frequency = frequency_y)
+      start_testing <- tsp(training)[2] + 1 / frequency_y
+      return(list(training = training,
+                  testing = ts(y[-index_train, ],
+                               start = start_testing,
+                               frequency = frequency_y)))
+    }
+}
+splitts  <- compiler::cmpfun(splitts)
+
