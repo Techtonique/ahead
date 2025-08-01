63942db29e4028bce827bd974db6574bc3ae37b6
Thierry Moudiki <thierry.moudiki@gmail.com>
2025-08-01 11:30:06 +0200

    explicitly add clusters to mlarch

--- R/mlarch.R
+++ R/mlarch.R
@@ -14,6 +14,9 @@
 #' @param B Number of bootstrap replications or simulations
 #' @param ml If \code{TRUE}, \code{fit_func} and \code{predict_func} are used, otherwise a statistical model in \code{stat_model}
 #' @param stat_model A statistical model, e.g \code{forecast::thetaf} or \code{forecast::auto.arima}
+#' @param n_clusters Number of clusters for residuals (default is 0) for \code{ml == TRUE} and \code{ml_method} not \code{NULL}
+#' @param clustering_dist Only if \code{n_clusters > 0}. If "euclidean", then mean square error, if "manhattan ", the mean absolute error is used.
+#' @param clustering_method Only if \code{n_clusters > 0}. If "kmeans", then we have the kmeans clustering method, if "hardcl" we have the On-line Update (Hard Competitive learning) method, and if "neuralgas", we have the Neural Gas (Soft Competitive learning) method. Abbreviations of the method names are accepted.
 #' @param ... Additional parameters to be passed to \code{stat_model}
 #'
 #' @return A forecast object containing predictions and prediction intervals
@@ -38,6 +41,9 @@ mlarchf <- function(y, h=10L,
                     B=250L,
                     ml=TRUE,
                     stat_model=NULL,
+                    n_clusters=0,
+                    clustering_dist="euclidean",
+                    clustering_method="kmeans",
                     ...)
 {
   if (!is.ts(y))
@@ -55,21 +61,55 @@ mlarchf <- function(y, h=10L,
 
   if (ml == TRUE)
   {
-      if (!is.null(ml_method))
-    {
-        fit_func <- function(x, y, method = ml_method, ...)
-      {
-        df <- data.frame(y=y, as.matrix(x)) 
+    if (!is.null(ml_method)) {
+      
+      fit_func <- function(x, y, method = ml_method, ...) {
+        
+        df <- data.frame(y = y, as.matrix(x)) 
         colnames(df) <- c("y", paste0("X", 1:ncol(x)))
-        caret::train(y ~ ., data=df,
-                    method = ml_method,
-                    trControl=caret::trainControl(method = "none"))
+        
+        if (n_clusters > 0) {
+          scaler_x <- misc::scale_matrix(as.matrix(x))
+          # Cluster only on the scaled features (no zero column needed)
+          clustering_obj <- cclust::cclust(cbind(as.matrix(scaler_x$X), 0), 
+                                           centers = n_clusters, 
+                                           dist = clustering_dist, 
+                                           method = clustering_method)
+          # Get cluster assignments (same number as original data)
+          predict_clusters <- predict(clustering_obj, cbind(as.matrix(scaler_x$X), 0))$cluster
+          # Ensure we only use the first nrow(df) assignments
+          df$cluster <- as.numeric(predict_clusters[1:nrow(df)])
+        }
+        
+        obj <- caret::train(y ~ ., data = df,
+                            method = ml_method,
+                            trControl = caret::trainControl(method = "none"))
+        
+        if (n_clusters > 0) {
+          obj$scaler_x <- scaler_x
+          obj$clustering_obj <- clustering_obj
+        }
+        return(obj)
       }
+      
+      predict_func <- function(obj, newx) {
+        newx <- as.matrix(newx)
+        colnames(newx) <- paste0("X", 1:ncol(newx))
 
-      predict_func <- function(obj, newx)
-      {
-        colnames(newx) <- paste0("X", 1:ncol(newx)) 
-        predict(obj, newx) 
+        if (n_clusters > 0) {
+          # Scale new data using original scaling parameters
+          newx_scaled <- misc::scale_matrix(as.matrix(newx), 
+                               X_mean = obj$scaler_x$X_mean, 
+                               X_sd = obj$scaler_x$X_sd)$X
+          # Predict clusters for new data
+          new_clusters <- predict(obj$clustering_obj, 
+                                  cbind(newx_scaled, 0))$cluster
+          # Combine with newx
+          newx <- data.frame(newx, cluster = as.numeric(new_clusters))
+          #colnames(newx) <- c(paste0("X", 1:ncol(newx)), "cluster")
+        }
+  
+        return(predict(obj, newx))
       }
     }
 
